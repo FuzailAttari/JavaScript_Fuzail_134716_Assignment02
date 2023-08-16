40fc6ce99910864a613a0e7645ecd40ee9b6d5b4
Fuzail Raza Attari <hunailattari@gmail.com>
2023-08-16 22:42:26 +0500

    JavaScript_Fuzail_134716_Assignment02

---
+++ App.js
@@ -0,0 +1,105 @@
+    //     ALERTS              
+    // // Alert Assigment
+    // // Assignment # 1
+    
+
+    // // Q1 
+// alert("Salaam");
+
+// // // Q2 
+// alert("Hello\nHow are you")
+
+// // // // Q3
+// alert('Hello\nHow are you?')
+// console.log("Hello\nHow are you?")
+
+
+// <--------------------------------------------------------------------------------->
+
+// VARIABLES FOR STRINGS
+//    Assignment # 2
+//     JAVASCRIPT
+
+// var firstname= "Fuzail";
+// alert (firstname);
+
+// var scdname= "Manai";
+// alert (scdname);
+
+
+// var Data= ("A smarter way to learn JavaScript");
+// alert ("I am Trying to LearnFrom the Book" +Data );
+// console.log
+
+
+// <--------------------------------------------------------------------------------->
+
+// // VARIABLES FOR NUMBERS
+//      Assignment # 3
+//       JAVASCRIPT
+
+    // var age =( "I am 32 year old");
+    // alert(age);
+
+    // var birthyear=1991 ;
+    // alert("My birth year is "+birthyear);  
+
+    // var visName = "Fuzail";
+    // var numTshirts = "5 T-shirt";
+    // var store = "XYZ Clothing Store"
+
+    // document.write(visName + " ordered " + numTshirts + "(s) on " + store)
+
+// <--------------------------------------------------------------------------------->
+
+    // VARIABLE NAMES: LEGAL & ILLEGAL
+    // Assignment # 4
+    // JAVASCRIPT
+
+    // document.write (" <h1>Rules for naming JS variables</h1>")
+
+    // var dash1 = "Fuzail";
+    // var dash2 ="1212";
+    // var dash3 = "$";
+    // var dash4= "SMIT";
+    // document.write (" Variable names can only contain " + dash1 + " " + dash2 + " " + dash3 + " and " + dash4 )
+
+    
+    // var dash5 = " Sensitive";
+    // document.write ( " </br> Variable names are case " +dash5 )
+    // var dash5 = " Keywords";
+    // document.write ( " </br> Variable names should not be JS " +dash5 )
+
+// <--------------------------------------------------------------------------------->
+
+            // MATH EXPRESSIONS    
+            // Assignment # 5
+            //   JAVASCRIPT
+
+            // var num= 3;
+            // var num1= 5;
+            // var num3= num+num1;
+            // document.write (" sum of " + num +" "+ "and " + num1 +  " is" + " "  + num3)
+            // var num= 3;
+            // var num1= 5;
+            // var num3= num*num1;
+            // document.write (" </br>  sum of " + num +" "+ "Multiply " + num1 +  " is" + " "  + num3)
+            // var num= 10;
+            // var num1= 2;
+            // var num3= num/num1;
+            // document.write (" </br>  sum of " + num +" "+ "Divied " + num1 +  " is" + " "  + num3)
+
+
+
+            document.write (" <h1> Table Of 4 </h1>")
+            var n1 = 4 ;
+            document.write ( " </br> 4 x 1 = " + n1 * 1 )
+            document.write ( " </br> 4 x 1 = " + n1 * 2 )
+            document.write ( " </br> 4 x 1 = " + n1 * 3 )
+            document.write ( " </br> 4 x 1 = " + n1 * 4 )
+            document.write ( " </br> 4 x 1 = " + n1 * 5 )
+            document.write ( " </br> 4 x 1 = " + n1 * 6 )
+            document.write ( " </br> 4 x 1 = " + n1 * 7 )
+            document.write ( " </br> 4 x 1 = " + n1 * 8 )
+            document.write ( " </br> 4 x 1 = " + n1 * 9 )
+            document.write ( " </br> 4 x 1 = " + n1 * 10 )
