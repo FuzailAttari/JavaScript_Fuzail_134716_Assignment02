6798b96549d118ce7aa19f8631d2e8baf507558c
Fuzail Raza Attari <hunailattari@gmail.com>
2023-08-16 22:54:19 +0500

    update

--- App.js
+++ App.js
@@ -1,105 +1,268 @@
-    //     ALERTS              
-    // // Alert Assigment
-    // // Assignment # 1
-    
-
-    // // Q1 
-// alert("Salaam");
-
-// // // Q2 
-// alert("Hello\nHow are you")
-
-// // // // Q3
-// alert('Hello\nHow are you?')
-// console.log("Hello\nHow are you?")
-
-
-// <--------------------------------------------------------------------------------->
-
-// VARIABLES FOR STRINGS
-//    Assignment # 2
-//     JAVASCRIPT
-
-// var firstname= "Fuzail";
-// alert (firstname);
-
-// var scdname= "Manai";
-// alert (scdname);
-
-
-// var Data= ("A smarter way to learn JavaScript");
-// alert ("I am Trying to LearnFrom the Book" +Data );
-// console.log
-
-
-// <--------------------------------------------------------------------------------->
-
-// // VARIABLES FOR NUMBERS
-//      Assignment # 3
-//       JAVASCRIPT
-
-    // var age =( "I am 32 year old");
-    // alert(age);
-
-    // var birthyear=1991 ;
-    // alert("My birth year is "+birthyear);  
-
-    // var visName = "Fuzail";
-    // var numTshirts = "5 T-shirt";
-    // var store = "XYZ Clothing Store"
-
-    // document.write(visName + " ordered " + numTshirts + "(s) on " + store)
-
-// <--------------------------------------------------------------------------------->
-
-    // VARIABLE NAMES: LEGAL & ILLEGAL
-    // Assignment # 4
-    // JAVASCRIPT
-
-    // document.write (" <h1>Rules for naming JS variables</h1>")
-
-    // var dash1 = "Fuzail";
-    // var dash2 ="1212";
-    // var dash3 = "$";
-    // var dash4= "SMIT";
-    // document.write (" Variable names can only contain " + dash1 + " " + dash2 + " " + dash3 + " and " + dash4 )
-
-    
-    // var dash5 = " Sensitive";
-    // document.write ( " </br> Variable names are case " +dash5 )
-    // var dash5 = " Keywords";
-    // document.write ( " </br> Variable names should not be JS " +dash5 )
-
-// <--------------------------------------------------------------------------------->
-
-            // MATH EXPRESSIONS    
-            // Assignment # 5
-            //   JAVASCRIPT
-
-            // var num= 3;
-            // var num1= 5;
-            // var num3= num+num1;
-            // document.write (" sum of " + num +" "+ "and " + num1 +  " is" + " "  + num3)
-            // var num= 3;
-            // var num1= 5;
-            // var num3= num*num1;
-            // document.write (" </br>  sum of " + num +" "+ "Multiply " + num1 +  " is" + " "  + num3)
-            // var num= 10;
-            // var num1= 2;
-            // var num3= num/num1;
-            // document.write (" </br>  sum of " + num +" "+ "Divied " + num1 +  " is" + " "  + num3)
-
-
-
-            document.write (" <h1> Table Of 4 </h1>")
-            var n1 = 4 ;
-            document.write ( " </br> 4 x 1 = " + n1 * 1 )
-            document.write ( " </br> 4 x 1 = " + n1 * 2 )
-            document.write ( " </br> 4 x 1 = " + n1 * 3 )
-            document.write ( " </br> 4 x 1 = " + n1 * 4 )
-            document.write ( " </br> 4 x 1 = " + n1 * 5 )
-            document.write ( " </br> 4 x 1 = " + n1 * 6 )
-            document.write ( " </br> 4 x 1 = " + n1 * 7 )
-            document.write ( " </br> 4 x 1 = " + n1 * 8 )
-            document.write ( " </br> 4 x 1 = " + n1 * 9 )
-            document.write ( " </br> 4 x 1 = " + n1 * 10 )
+App.js
+// //  ==========MATH EXPRESSIONS===============
+//  //  ========== Q-1 ===============
+// var a =  10 ;
+// document.write ( "  <h1> Result </h1>")
+// document.write ( "  The Value of a is:" +" "+ a)
+// document.write ( "  </br> " )
+// document.write ( "  </br> The Value of ++a is:" +" "+ ++a)
+// document.write ( "  </br>  Now The Value of a is:" +" "+ a)
+// document.write ( "  </br> " )
+// document.write ( "  </br> The Value of ++a is:" +" "+ a)
+// document.write ( "  </br>  Now The Value of a is:" +" "+ ++a)
+// document.write ( "  </br> " )
+// document.write ( "  </br> The Value of --a is:" +" "+ --a)
+// document.write ( "  </br>  Now The Value of a is:" +" "+ a)
+// document.write ( "  </br> " )
+// document.write ( "  </br> The Value of a-- is:" +" "+ a)
+// document.write ( "  </br>  Now The Value of a is:" +" "+ --a)
+//  //  ========== Q-2 ===============
+//  var a = 2 ;
+//  var b = 1 ; 
+//  var res = --a - --b + ++b + b--;
+//  var res2 = --a;
+//  var res3 = --a - --b;
+//  var res4 = --a - --b + ++b;
+//  var res5 = --a - --b + ++b + b--;  
+//  document.write ("a is " + " "+ "2")
+//  document.write (" </br>")
+//  document.write (" </br> b is " + " "+ "1")
+//  document.write (" </br>")
+//  document.write ( "  </br> --a - --b + ++b + b--" + " "+ "Result is  "+" "+ res ) ;
+//  document.write (" </br>")
+//  document.write ( " </br> --a"+" " +" Result is "+" "+ res2 ) ; 
+//  document.write (" </br>")
+//  document.write ( " </br> --a - --b" +" " + " Result is "+" "+ res3 ) ;
+//  document.write (" </br>")
+//  document.write ( " </br>  --a - --b + ++b "+  " "+"Result is "+" "+ res4 ) ;
+//  document.write (" </br>")
+//  document.write ( " </br> --a - --b + ++b + b--" +" " + " Result is "+" "+ res5 ) ;
+//  //  ========== Q-3 ===============
+//   var usergreet = prompt("Enter your Name")
+//   document.write (usergreet)
+//   var userName = prompt("Enter your Name")
+//   document.write (userName)
+//  //  ========== Q-4 & Q-5 ===============
+//       var num1 = +prompt  ( 'enter your value') 
+// var num2 = +prompt  ( 'enter your next value')
+// var Add = num1 + num2 
+// var num3 = prompt  ( 'enter your value') 
+// var num4 = prompt  ( 'enter your next value')
+// var Minus  = num3 - num4
+// var num5 = prompt  ( 'enter your value') 
+// var num6 = prompt  ( 'enter your next value')
+// var Multiplu = num5 * num6
+// var num7 = prompt  ( 'enter your value') 
+// var num8 =+prompt  ( 'enter your next value')
+// var Divied = num7 / num8
+// var num9 = prompt  ( 'enter your value') 
+// var num10 = prompt  ( 'enter your next value')
+// var Modus = num9 % num10
+// document.write ( 
+// '<table border = "2px">'+ 
+// '<tr>'+
+// '<th>'+'Additon'+'</th>'+
+// '<th>'+'Minus'+'</th>'+
+// '<th>'+'Multiplu'+'</th>'+
+// '<th>'+'Divied'+'</th>'+
+// '<th>'+'Modus'+'</th>'+'</tr>'+ 
+// '<tr>'+
+// '<td>'+ Add + '</td>' +
+// '<td>'+ Minus + '</td>' +
+// '<td>'+ Multiplu + '</td>' +
+// '<td>'+ Divied + '</td>' +
+// '<td>'+ Modus+ '</td>' +
+// '</tr>'
+// +"</table>"
+// )
+//  //  ========== Q-6 ===============
+// var Name = prompt( "Enter Your Name", "Fuzail" ) 
+// var subject1 = prompt ("Enter the name of the first subject"," Maths");
+// var subject2 = prompt ("Enter the name of the second subject", "Physics");
+// var subject3 = prompt ("Enter the name of the third subject", "Urdu");
+// document.write( "<h1>  Fuzail raza</h1>"   );
+// var marks1 = prompt ( "enter marks" )
+// var marks2 = prompt ( "enter marks" )
+// var marks3 = prompt ( "enter marks" )
+// var sub = marks1 + marks2 +marks3 ;
+// var totalMarks = 100;
+// var subtotal    = 300;
+// var Percentage = ( sub / subtotal ) * 100 ;
+// document.write ( 
+// '<table border = "2px">'+ 
+// '<tr>'+
+// '<th>'+'Subject'+'</th>'+
+// '<th>'+'Total Marks'+'</th>'+
+// '<th>'+'Obtained Marks'+'</th>'+
+// '<th>'+'Percentage'+'</th>'+
+// '</tr>'
+// +'<tr>'+
+// '<td>'+ subject1+ '</td>' +
+// '<td>'+ totalMarks+ '</td>' +
+// '<td>'+ marks1+ '</td>' +
+// '<td>'+ Percentage+ '</td>' +'</tr>'
+// +'<tr>'+
+// '<td>'+ subject2+ '</td>' +
+// '<td>'+ totalMarks+ '</td>' +
+// '<td>'+ marks2+ '</td>' +
+// '<td>'+ Percentage+ '</td>' +'</tr>'
+// +'<tr>'+
+// '<td>'+ subject3+ '</td>' +
+// '<td>'+ totalMarks+ '</td>' +
+// '<td>'+ marks3+ '</td>' +
+// '<td>'+ Percentage+ '</td>' +'</tr>'
+// +"</table>"
+// )
+// //  ==========USER INPUT & CONDITIONAL STATEMENT===============
+//  //  ========== Q-1  ===============
+// var userInput = prompt ( " Enter Your City" , )
+// if ( userInput === "Karachi"  ){
+// document.write(" Welcome to city of lights")}
+// else  {
+//     document.write( " Aby ja yaha se apne Shehar ")
+// }
+//  //  ========== Q-2  ===============
+//     var gender = prompt( " Enter Gender")
+//     if ( gender === "male"){
+//         document.write("Good Morning Sir") 
+//     }
+//    else if ( gender === "female"){
+//         document.write("Good Morning Madam") 
+//     }
+//     else {
+//         document.write( "Error ")
+//     }
+//  //  ========== Q-3  ===============
+// var signalLight = prompt ( "enter color" )
+// if ( signalLight === "red") {
+//     window.alert  ("Must Stop") 
+// }
+// else if ( signalLight === "yellow"){
+//     window.alert("Ready to move")
+// }
+// else if (signalLight === "green"){
+//     window.alert("Move Now");
+// }
+//  //  ========== Q-6    ===============
+// var eng = +prompt("Please Enter Your English Subject Obtained Marks");
+// var mat = +prompt("Please Enter Your Math Subject Obtained Marks");
+// var urdu = +prompt("Please Enter Your Urdu Subject Obtained Marks");
+// var totalMarks = 300;
+// var totalObtainedMarks = eng + mat + urdu;
+// var percentage = (totalObtainedMarks / totalMarks) * 100;
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("<br/> Total Marks : " + totalMarks);
+// document.write("<br/> Marks Obtained : " + totalObtainedMarks);
+// document.write("<br/> Percentage : " + percentage + "%");
+// if (percentage >= 80) {
+//   document.write(" <br/> Grade : A-One");
+//   document.write(" <br/> Remarks : Excellent");
+// } else if (percentage >= 70) {
+//   document.write(" <br/> Grade : A");
+//   document.write(" <br/> Remarks : Good");
+// } else if (percentage >= 60) {
+//   document.write(" <br/> Grade : B");
+//   document.write(" <br/> Remarks : You need to imporve");
+// } else if (percentage < 60) {
+//   document.write(" <br/> Grade : Fail");
+//   document.write(" <br/> Remarks : Sorry");
+// }
+//  //  ========== Q-7 Guess game:   ===============
+// var guessNumber = prompt( "enter number ranging from 1 to 10" )
+// var secretNumber = 7
+// if ( secretNumber === guessNumber++)
+// {
+//     alert( "Bingo! Correct answer")
+// }
+// else   {
+//     alert("Wrong guess")
+// }
+//  //  ========== Q-8  ===============
+// var userInput = +prompt("Enter a number");
+// if (userInput % 3 === 0){   
+//    alert("The Number is divisible by 3");
+// } else{
+//    alert("The Number is not divisible by 3");
+// }
+//  //  ========== Q-9  ===============
+// var userInput = prompt("Enter anumber to number is either odd or even");
+// if (userInput % 2 === 0){   
+//        alert("Number is even");
+//     } else{
+//        alert("Number is odd");
+//     }
+//  //  ========== Q-10  ===============
+// var userInput = prompt("Enter Temperature");
+// if(userInput >= 40){
+//    alert("It is too hot outside");
+// } else if(userInput >= 30 && userInput < 40) {
+//    alert("The weather today is Normal");
+// } else if(userInput >= 20 && userInput < 30){
+//    alert("Today's weather is cool");
+// } else {
+//    alert("OMG! Today's weather is so cool");
+// }
+//  //  ========== Q-11   ===============
+// var num1 = +prompt  ( 'enter your Additon value') 
+// var num2 = +prompt  ( 'enter your next Additon value')
+// var Add = num1 + num2 
+// var num3 = prompt  ( 'enter your Minus value') 
+// var num4 = prompt  ( 'enter your next Minus value')
+// var Minus  = num3 - num4
+// var num5 = prompt  ( 'enter your   Multiplu value') 
+// var num6 = prompt  ( 'enter your next Multiplu  value')
+// var Multiplu = num5 * num6
+// var num7 = prompt  ( 'enter your Divied  value') 
+// var num8 =+prompt  ( 'enter your next Divied  value')
+// var Divied = num7 / num8
+// var num9 = prompt  ( 'enter your Modus value') 
+// var num10 = prompt  ( 'enter your next Modus value')
+// var Modus = num9 % num10
+// document.write ( 
+// '<table border = "2px">'+ 
+// '<tr>'+
+// '<th>'+'Additon'+'</th>'+
+// '<th>'+'Minus'+'</th>'+
+// '<th>'+'Multiplu'+'</th>'+
+// '<th>'+'Divied'+'</th>'+
+// '<th>'+'Modus'+'</th>'+'</tr>'+ 
+// '<tr>'+
+// '<td>'+ Add + '</td>' +
+// '<td>'+ Minus + '</td>' +
+// '<td>'+ Multiplu + '</td>' +
+// '<td>'+ Divied + '</td>' +
+// '<td>'+ Modus+ '</td>' +'</tr>'
+// +"</table>"
+// )
+// ============ // IF...ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS============
+//    ==================== chapter 12-13===================
+//  //  ========== Q-1   ===============
+// var userInputVal = prompt("Type a Keyword");
+// if (userInputVal >= "A" && userInputVal <= "Z") {
+//     document.write("This is an UPPERCASE")
+// } else if (userInputVal >= "a" && userInputVal <= "z") {
+//     document.write("This is an lowercase")
+// } else {
+//     document.write("Value is not a string")
+// }
+// /  / /  ========== Q - 2 ===============
+// var userInputInt1 = prompt("Type Integer 1");
+// var userInputInt2 = prompt("Type Integer 2");
+// if (userInputInt1 < userInputInt2) {
+//     document.write("Integer 2 " + userInputInt2 + " is Greater")
+// } else if (userInputInt1 > userInputInt2) {
+//     document.write("Integer 1 " + userInputInt1 + " is Greater")
+// } else {
+//     document.write("Both integers are equal")
+// }
+// /  / /  ========== Q - 3 ===============
+var usernum = +prompt("Enter number")
+if (usernum === 0) {
+    document.write(" the number is zero")
+}
+else if (usernum > 0 ) {
+    document.write( "ther number is positive")
+}
+else {
+    document.write( "the number is nagetive")
+}
